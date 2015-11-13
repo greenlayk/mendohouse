0f1709947989c0fb1ee114dce54c66b7b83015aa
Bruce Green <greenlayk@gmail.com>
2015-11-12 20:06:11 -0600

    add FlipKey Best of Badge

--- index.php
+++ index.php
@@ -4,7 +4,30 @@
     <meta charset="UTF-8">
     <title>Sea & River House</title>
 </head>
-<body>
-    The views are spectacular!
-</body>
-</html>
+  <body>
+      The views are spectacular!
+    <footer>
+      <div id="FK_Best_Of_Badge533b020ded110" class=" svg">
+       <div class="fk-best-of-badge flipkey-reset">
+        <div class="fk-star-icon">
+         <img src="https://s1.fkimg.com/img/widgets/silver.png">
+        </div>
+            <div class="fk-badge-content">
+             <div class="fk-header">
+              Top Vacation Rental
+             </div>
+             <div class="fk-badge-text">
+                 <p class="fk-property_title" onclick="<window class="FlipKey"></window>fk_frontdesk_link('25524')">SEA &amp; RIVER H o u s e
+                 </p>
+             </div>
+               <span class="fk-logo">
+                <a target="_blank" href="https://www.flipkey.com/">
+                 <img alt="FlipKey" src="https://s1.fkimg.com/img/FK-logo-90x14.png">
+                </a>
+               </span>    
+            </div><!-- fk-badge-content -->
+            </div><!-- fk-best-of-badge -->
+      </div><!-- FK_Best_Of_Badge533b020ded110 -->
+    </footer>
+  </body>
+  </html>
