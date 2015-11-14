2e236117ac1b472652c3313b15d8e4e2d1208593
Bruce Green <greenlayk@gmail.com>
2015-11-13 22:26:05 -0600

    added FlipKey calendar and Best of features

--- index.php
+++ index.php
@@ -6,6 +6,41 @@
 </head>
   <body>
       The views are spectacular!
+      <article>
+        <div id="FK_Calendar_Widget53439ff4badb7" style="width:400px; margin:25px auto 0 auto; text-shadow:none;">
+        <a href="http://www.flipkey.com/" mce_href="http://www.flipkey.com/"><img alt="FlipKey" src="http://www.flipkey.com/img/widgets/FlipKey-118x29.png" mce_src="http://www.flipkey.com/img/widgets/FlipKey-118x29.png" class="fk-calendar"></a>
+    </div>
+      </article>
+      <article>
+        <div class="col-sm-6">
+          <div class="caption">
+            <h3><span class="label label-info">Contact</span></h3>
+              <iframe src="https://www.youtube.com/embed/5gydT0b1Lmw?rel=0" width="231" height="155" style="border: none;"></iframe>
+          </div><!-- caption -->
+        </div>
+      </article>
+    <article class=albion-map>
+       <script> 
+          function init_map() {
+          var var_location = new google.maps.LatLng(45.430817,12.331516);
+       
+              var var_mapoptions = {
+                center: var_location,
+                zoom: 14
+              };
+       
+          var var_marker = new google.maps.Marker({
+            position: var_location,
+            map: var_map,
+            title:"Venice"});
+       
+              var var_map = new google.maps.Map(document.getElementById("map-container"),
+                  var_mapoptions);
+                  var_marker.setMap(var_map); 
+              }
+          google.maps.event.addDomListener(window, 'load', init_map);
+        </script>
+     </article>
     <footer>
       <div id="FK_Best_Of_Badge533b020ded110" class=" svg">
        <div class="fk-best-of-badge flipkey-reset">
@@ -30,4 +65,4 @@
       </div><!-- FK_Best_Of_Badge533b020ded110 -->
     </footer>
   </body>
-  </html>
+</html>
