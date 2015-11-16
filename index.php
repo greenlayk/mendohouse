15691f2f8e9aac7360afcf5a20bb54ba9c805e5b
Bruce Green <greenlayk@gmail.com>
2015-11-15 20:27:25 -0600

    updated file source and body content

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
     <title>Bootstrap 101 Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
-    <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
+    <link href="_/css/custom.css" rel="stylesheet" media="screen">
   </head>
   <body>
     <h1>Hello, world!</h1>
+      <p>The time is now</p>
 
     <script src="_/js/bootstrap.js"></script>
-    <script src="_/js/myscript.js"></script>
+    <script src="_/js/customScript.js"></script>
   </body>
 </html>
