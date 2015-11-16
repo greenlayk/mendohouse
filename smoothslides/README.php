4e2277ea03041d560f538552c771bf4bf828466a
Bruce Green <greenlayk@gmail.com>
2015-11-15 21:10:49 -0600

    smoothslide content update

---
+++ smoothslides/README.php
@@ -0,0 +1,20 @@
+<h1 id="smoothslides">Smoothslides</h1>
+<p>A responsive jQuery slideshow with beautiful Ken Burns type effects on each image. Created by <a href="https://twitter.com/kthornbloom" target="_blank">Kevin Thornbloom</a>
+<br>
+<a href="http://kthornbloom.github.io/Smoothslides/" target="_blank">Demo &amp; Docs</a></p>
+<h1 id="updates">Updates</h1>
+<p>V2.2.1</p>
+<ul>
+<li>Fixed bug where effectModifier caused some images to go off screen</li>
+</ul>
+<p>V2.2.0</p>
+<ul>
+<li>New option - order</li>
+<li>New option - effectModifier</li>
+<li>Ability to set certain effects on certain images</li>
+</ul>
+<p>V2.1.0</p>
+<ul>
+<li>New option - matchImageSize</li>
+<li>Adding limited support for IE8</li>
+</ul>
