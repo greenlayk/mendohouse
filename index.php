91e768bf2f0221135e209ad03a75e2344d06cac8
Bruce Green <greenlayk@gmail.com>
2015-11-23 13:49:35 -0600

    add smoothslide functionality

--- index.php
+++ index.php
@@ -6,14 +6,25 @@
     <link href="http://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Ubuntu:400,500" rel="stylesheet" type="text/css">
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="_/css/smoothslides.theme.css">
     <link href="_/css/custom.css" rel="stylesheet" media="screen">
   </head>
   <body id="home" data-spy="scroll" data-target="active">
   <section class="container">
     <section class="content row">
-      
+
+      <span class="header-img"><?php  include "_/components/php/header.php"; ?></span><!-- header-img -->
+         <div class="smoothslides" id="myslideshow1">
+          <img src="images/views-from-house/carousel/from-the-house-01.jpg"/>
+          <img src="images/views-from-house/carousel/from-the-house-02.jpg"/>
+          <img src="images/views-from-house/carousel/10.jpg"/>
+          <img src="images/views-from-house/carousel/11.jpg"/>
+         </div><!-- smoothslides -->
+
        <section class="main col col-lg-8">
-         <?php  include "_/components/php/header.php"; ?>
+         
+
+
        </section><!--main col col-lg-8  -->
        <section class="sidebar col col-lg-4">
        </section><!-- sidebar col col-lg-4 -->
@@ -22,6 +33,13 @@
     
 
     <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/jquery.js"></script>
+    <script src="_/js/smoothslides-2.2.1.min.js"></script>
+    <script>
+      $(window).load( function() {
+      $('#myslideshow1').smoothSlides();
+      });
+    </script>
     <script src="_/js/customScript.js"></script>
     <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
   </body>
