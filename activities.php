157cf6bea10f2960df72a9d68e8626a38323d926
Bruce Green <greenlayk@gmail.com>
2015-11-28 20:06:16 -0600

    add text content to activities.php and modify custom.css to accommodate changes

--- activities.php
+++ activities.php
@@ -13,8 +13,46 @@
       <div class="content row">
       <?php  include "_/components/php/header.php"; ?>
         <section class="main col col-lg-8">
-          
+          <article class="dining">
+            <p>
+              There is <a href="http://www.mendocino.com/restaurants_all.html">a selected list of good restaurants </a>in the Mendocino vicinity with further links to each establishment's own web page where you can find their menus. A more comprehensive list, although with only addresses and phone numbers, and no descriptions, can be found <a href="http://www.mendocino.winecountry.com/thingstodo/dining/north_coastlistings.html">here.</a><br>  
+              Among our favorites are the Mendocino Cafe in Mendocino and the Little River Inn and the Ledford House on the highway from Sea & River House up to Albion, and Silvers in Fort Bragg.
+            </p>
+          </article>
         </section>
+
+        <section class="main col col-lg-8">
+          <article class="do">
+            <h4>Biking</h4>
+            <p class="biking">
+              Not best done on Hwy. 1, as there is no shoulder. Most of the parks have paved paths suitable for biking. Most outfitters rent bikes, touring and mountain. Dirt bike and mountain biking can be done at Cow Mountain Recreation Area. www.StanfordInn.com advertises that it outfits mountain bikes, kayaks, and canoes including outrigger canoes.
+            </p><!-- biking -->
+
+            <h4>Fishing</h4>
+            <p>
+              Fishing There are many deep sea fish, including rockfish, lingcod, halibut, tuna and chinook salmon, plus crab to be caught off of charter boats out of Noyo Harbor in Fort Bragg. www.allaboardadventures.com. Near Point Arena you can book a full day adventure with professional sportfishers for a small (2-4) group and even hunt abalone if appropriate. Equipment and lunch is provided. www.fairchasesportfishing.com. Crab season is in January. Point Arena has a 330 ft. pier over a cove famous for fishing.
+            </p>
+
+            <h4>Golf</h4>
+            <p>
+               Some of the inns offer golf: Little River Inn and the Seafoam Lodge both have a 9 hole course. www.seafoamlodge.com. miniature golf is offered at Emerald Dolphin Mini Golf at the edge of Fort Bragg. Disc golf, also know as frisbee golf, is available at Anderson Valley Brewing Co. ( corner of Hwy. 253 and 128 at Boonville) and you can have a beer as well. <a href="www.cityofukiah.com">Ukiah</a>  has a municipal 18 hole course and swimming pools.
+              </p>
+
+              <h4>Horseback Riding</h4>   
+              <p>Ricochet Ranch at the northern edge of Fort Bragg organizes rides for adults and children older than 6 through the forest and into McKerricher Park beach areas lasting from 1.5 hours to all day rides. www.horse-vacation.com. <a href="www.rossranch.biz">Ross Ranch</a>organizes private rides through the redwoods or on the beach on Manchester Beach.
+              </p>
+
+              <h4>Kayaking/Canoeing</h4>   
+              <p>Several outfitters along the coast provide river and ocean kayaking. All equipment is provided. Force 10 in Elk (707-877-3505/ www.force10tours.com ) has kayaks while Catch a Canoe at Hwy 1 and Comptche Rd. ((707-937-0273) offers kayaks, mountain bikes and outrigger canoes. Kayak Mendocineo (707-964-7480/www.kayakmendocino.com) kayaks through coastal caves and past seal covered rocks, starting out at Van Damme State Park
+              </p>
+
+             <h4>Nature Walks</h4>
+             <p>Hike the dunes and seven miles of beach in McKerricher State Park which begins 3 miles north of Fort Bragg. Camp sites are available and beautiful beach plant life. Remains of old logging roads can be hiked or biked. Climb the pygmy forest of stunted cypress and pines off Hwy 1, 3 miles south of Mendocino. Walk through cool fern covered canyon in Van Damme State Park where Little River empties into the ocean. Russian River Gulch State Park has a stunning surf with tide pools but also waterfall and blow hole created when a 200 ft tunnel collapsed into itself. Big River Park has guided nature tours during the spring and summer. Navarro River Redwoods State Park has many spectacular views and swimming holes but shorter hikes from the highway to the river.
+              </p> 
+
+          </article>
+        </section>
+
         <section class="sidebar col col-lg-4">
         
         </section>
